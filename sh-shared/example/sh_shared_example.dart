7adbfd6b5c81565c96837a6c10c412b304b46f94
Dominik Baron <domo.baron@aon.at>
2018-05-29 08:55:20 +0200

    add sh-shared project

---
+++ sh-shared/example/sh_shared_example.dart
@@ -0,0 +1,6 @@
+import 'package:sh_shared/sh_shared.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ sh-shared/lib/sh_shared.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library sh_shared;
+
+export 'src/sh_shared_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
