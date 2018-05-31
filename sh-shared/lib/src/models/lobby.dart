89e8fa6e1bfc22ca7e0aa27f43a880ea92a760f2
Dominik Baron <domo.baron@aon.at>
2018-05-31 15:21:29 +0200

    add open attribute to lobby and check if lobby is open before added player

--- sh-shared/lib/src/models/lobby.dart
+++ sh-shared/lib/src/models/lobby.dart
@@ -4,14 +4,24 @@ import 'serializable.dart';
 class Lobby extends Serializable {
   int id;
   String name;
-  List<Player> players;
+  List<Player> _players = new List<Player>();
+  bool open = true;
 
-
-  Lobby([int id, String name]) : this.withPlayers(id, name, new List<Player>());
-
-  Lobby.withPlayers(this.id, this.name, this.players);
+  Lobby([this.id, this.name]);
 
   Lobby.fromJson(Map<String, dynamic> jsonMap) : super.fromJson(jsonMap);
 
   Lobby.fromJsonString(String json) : super.fromJsonString(json);
+
+  List<Player> get player => _players;
+
+  void removePlayerWithId(int playerId) =>
+      _players.removeWhere((player) => player.id == playerId);
+
+  void addPlayer(Player player) {
+    if (!open) {
+      throw new Exception('Lobby is not open anymore - player can\'t join');
+    }
+    _players.add(player);
+  }
 }
