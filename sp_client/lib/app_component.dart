61a26ec03de91d55386f22c8434a1145d08ed1c8
Dominik Baron <domo.baron@aon.at>
2018-06-13 09:49:58 +0200

    change socket_io_service methods to use futures

--- sp_client/lib/app_component.dart
+++ sp_client/lib/app_component.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:sp_client/components/game/game.dart';
 import 'package:sp_client/services/game_state_service.dart';
 import 'package:sp_client/services/socket_io_service.dart';
+import 'package:sp_shared/sp_shared.dart';
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
@@ -14,11 +16,18 @@ import 'package:sp_client/services/socket_io_service.dart';
   directives: const [materialDirectives, GameComponent],
   providers: const [materialProviders, GameStateService, SocketIoService],
 )
-class AppComponent {
+class AppComponent implements OnInit {
 
-  AppComponent(SocketIoService service, GameStateService gameState) {
+  SocketIoService service;
+  SocketIoService gameState;
+
+  AppComponent(this.service, this.gameState) {
+  }
+
+  Future ngOnInit() async {
     service.setName('Test');
     service.createLobby('lob');
     Lobby l = await service.whenLobbyJoined();
+    print(l);
   }
 }
