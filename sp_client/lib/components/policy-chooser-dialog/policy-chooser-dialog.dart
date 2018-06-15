0a9ed33e0b9ed69b7a2742e458cfd10bffd79850
Dominik Baron <domo.baron@aon.at>
2018-06-16 00:30:59 +0200

    add policy chooser dialog component

---
+++ sp_client/lib/components/policy-chooser-dialog/policy-chooser-dialog.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+
+@Component(
+  selector: 'app-policy-chooser-dialog',
+  styleUrls: const ['policy-chooser-dialog.scss.css'],
+  templateUrl: 'policy-chooser-dialog.html',
+  directives: const [CORE_DIRECTIVES, materialDirectives],
+  providers: const [materialProviders],
+)
+class PolicyChooserDialogComponent implements OnInit {
+  String loyalistPolicyImgUrl = '/assets/images/policy/loyalistPolicy.gif';
+  String separatistPolicyImgUrl = '/assets/images/policy/seperatistPolicy.gif';
+
+  final _finishedDiscarding = new StreamController<List<bool>>();
+
+  @Output()
+  Stream<List<bool>> get finishedDiscarding => _finishedDiscarding.stream;
+
+  @Input()
+  List<bool> policies;
+
+  @Input()
+  bool showDialog = false;
+
+  @override
+  ngOnInit() async {}
+
+  void discardPolicy(bool policy) {
+    print('discard');
+
+    var found = false;
+    var remainingPolicies = policies.sublist(0);
+    remainingPolicies.removeWhere((bool policyElem) {
+      if (!found && policyElem == policy) {
+        found = true;
+        return true;
+      }
+      return false;
+    });
+    _finishedDiscarding.add(remainingPolicies);
+  }
+}
